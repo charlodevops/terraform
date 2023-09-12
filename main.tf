f6689b578303235ac0f4be23054fe266b48dc1b6
AGBORNTUI BATE <87149550+charlodevops@users.noreply.github.com>
2023-09-11 19:31:50 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+###########################################
+# IDC Lambda Configs
+###########################################
 provider "aws" {
   region = "eu-central-1"
 }
@@ -12,14 +15,8 @@ variable "python_code_path" {
   default     = "python/"
 }
 
-data "archive_file" "zip_the_python_code" {
-  type        = "zip"
-  source_dir  = var.python_code_path
-  output_path = "${path.module}/lambda_function_code.zip"
-}
-
 resource "aws_iam_role" "iam_role" {
-  name = "${var.lambda_function_name}_Role"
+  name = "${var.lambda_function_name}_Role3"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -37,7 +34,7 @@ resource "aws_iam_role" "iam_role" {
 }
 
 resource "aws_iam_policy" "iam_policy" {
-  name   = "LambdaFunctionPolicy"
+  name   = "LambdaFunctionPolicy3"
   path   = "/"
   policy = jsonencode({
     Version = "2012-10-17",
@@ -66,16 +63,43 @@ resource "aws_lambda_function" "function" {
 
   depends_on = [aws_iam_policy_attachment.attach_policy]
   layers = ["arn:aws:lambda:eu-central-1:017000801446:layer:AWSLambdaPowertoolsPythonV2:40"]
-    environment {
+  environment {
     variables = {
       key1 = "value1",
       key2 = "value2"
     }
   }
 
+  # Enable CloudWatch logging
+  tracing_config {
+    mode = "Active"
+  }
 }
 
-output "lambda_function_arn" {
-  description = "ARN of the created Lambda function"
-  value       = aws_lambda_function.function.arn
+# Create an HTTP API Gateway
+resource "aws_apigatewayv2_api" "example" {
+  name          = "IDC-API"
+  protocol_type = "HTTP"
+}
+
+# Define an HTTP integration for the Lambda function
+resource "aws_apigatewayv2_integration" "example" {
+  api_id          = aws_apigatewayv2_api.example.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = aws_lambda_function.function.invoke_arn
+  integration_method = "POST"
+  connection_type  = "INTERNET"
+  timeout_milliseconds = 29000
+}
+
+resource "aws_apigatewayv2_route" "custom_route" {
+  api_id    = aws_apigatewayv2_api.example.id
+  route_key = "GET /newendpoint"  # You can specify the HTTP method you want to use (e.g., GET, POST, ANY)
+  target    = "integrations/${aws_apigatewayv2_integration.example.id}"
+
+}
+
+output "api_gateway_url" {
+  description = "URL of the created API Gateway endpoint"
+  value       = aws_apigatewayv2_api.example.api_endpoint
 }
